535fadcf48e0dc4e01b60cf70a7ad4767a716783
cuevhv <cuevhv@yahoo.com>
2018-04-26 17:34:04 +0100

    bbx understanding

---
+++ creating_anchors.py
@@ -0,0 +1,26 @@
+network = "vgg16"
+bbx_size = [8, 16, 32]
+bbx_ratio = [0.5, 1, 1.5]
+bbx_image = list([[]])
+count1 = 0
+count2 = 0
+if network == "vgg16":
+    cell_size = 16
+    row_size = 224
+    col_size = 224
+    centers = list([[],[]])
+    for i in range (1, row_size+1, 16):
+        c_row = i+cell_size/2-1
+        centers[0].append(c_row)
+        bbx_image[count1].append(c_row)
+        for j in range (1, col_size+1, 16):
+            c_col = j+cell_size/2-1
+            if i == 1: centers[1].append(c_col)
+            for sz in [8]:
+                pass
+                #print "sz", sz
+            #print c_row, c_col, j+cell_size-1
+    print(centers)
+
+d = {}
+d[(8,8)] = 8
--- sudo_matlab/visualizing_anchors.m
+++ sudo_matlab/visualizing_anchors.m
@@ -1,12 +1,3 @@
-
-rectangle('Position',[0 -224 224 224])
-axis([0 250 -250 0])
-for i = 0:16:224-16
-    for j = 0:16:224
-        rectangle('Position',[i -j 16 16], 'EdgeColor', 'r')
-    end
-end
-
 close all
 sprintf('Visualizing grid cell in red')
 rectangle('Position',[0 -224 224 224])
