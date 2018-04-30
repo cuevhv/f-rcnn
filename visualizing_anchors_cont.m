c823ba912e083a70eca011804a19094f4d657265
cuevhv <cuevhv@yahoo.com>
2018-04-30 12:22:48 +0100

    modified matlab files

--- visualizing_anchors_cont.m
+++ visualizing_anchors_cont.m
@@ -12,20 +12,20 @@ pred_h = exp(0.54)*hp;
 
 
 [pred_x, pred_y, pred_w, pred_h]
-h1 = rectangle('Position',[pred_x-pred_w/2 pred_y-pred_h/2 pred_w pred_h], 'EdgeColor', [0 0.5 0]);
+h1 = rectangle('Position',[pred_x-pred_w/2 pred_y-pred_h/2 pred_w pred_h], 'EdgeColor', [0 0.5 0], 'LineWidth', 3);
 
 
-xp = 8+16*6; 
-yp = -8-16*6;
-wp = 32;
-hp = 32;
-
-
-pred_x = 0.25*wp+xp;
-pred_y = 0.25*hp+yp;
-pred_w = exp(0.54)*wp;
-pred_h = exp(0.54)*hp;
-
-
-[pred_x, pred_y, pred_w, pred_h]
-h1 = rectangle('Position',[pred_x-pred_w/2 pred_y-pred_h/2 pred_w pred_h], 'EdgeColor', [0 0 0.5]);
+% xp = 8+16*6; 
+% yp = -8-16*6;
+% wp = 32;
+% hp = 32;
+% 
+% 
+% pred_x = 0.25*wp+xp;
+% pred_y = 0.25*hp+yp;
+% pred_w = exp(0.54)*wp;
+% pred_h = exp(0.54)*hp;
+% 
+% 
+% [pred_x, pred_y, pred_w, pred_h]
+% h1 = rectangle('Position',[pred_x-pred_w/2 pred_y-pred_h/2 pred_w pred_h], 'EdgeColor', [0 0 0.5], 'LineWidth', 3);
